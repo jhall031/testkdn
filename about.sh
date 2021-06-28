108838ffee2bb41eb18bc2b317eeff8cccd4fa38
jhall031 <85783577+jhall031@users.noreply.github.com>
2021-06-29 02:33:59 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "# Script Auto Install SSH & OpenVPN                    #"
+echo -e "#-----------------------------------------------------#"
+echo -e "# For Debian 9 & 10 Buster 64 bit                          #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Modified by JAVpala                                            #"
+echo -e "#-----------------------------------------------------#"
+echo -e "# Thanks To                                                            #"
+echo -e "#-----------------------------------------------------#"
+echo -e "#                                                                             #"
+echo -e "# Allah                                                                    #"
+echo -e "# My Family                                                            #"
+echo -e "# Google                                                                #"
+echo -e "# JAVpala Community                                          #"
+echo -e "#                                                                             #"
+echo -e "#-----------------------------------------------------#"
+echo -e "================================================="
